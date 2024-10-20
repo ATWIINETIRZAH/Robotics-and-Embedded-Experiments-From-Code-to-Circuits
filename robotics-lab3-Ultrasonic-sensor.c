06422b119dd921f9f07972a25cd73263384aa7ed
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2024-10-20 20:49:45 +0300

    c program to show distance using ultra sonic sensor

---
+++ robotics-lab3-Ultrasonic-sensor.c
@@ -0,0 +1,36 @@
+
+// define pins to connect
+#define trigPin 6
+#define echoPin 7
+
+void setup() {
+    // ensure distance is printed to the serial monitor
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  float duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) * 0.0344;
+  
+  if (distance >= 50 || distance <= 2){
+    Serial.print("Distance = ");
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
+}
